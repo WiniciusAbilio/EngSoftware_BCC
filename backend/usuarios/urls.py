0eccb5a4ec7518884416e117516eb72cdfc5178e
Winicius Abilio <54330607+WiniciusAbilio@users.noreply.github.com>
2023-11-05 23:47:25 -0300

    Delete backend directory

--- backend/usuarios/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('processarLogin/', views.processar_login, name='processar_login'),
-    path('processarCadastroUsuario/', views.processar_cadastro_usuario, name='processar_cadastro_usuario'),
-]
