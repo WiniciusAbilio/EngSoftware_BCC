83782124f4b529ef7e9de297a5f243f01b5ba421
ANELLY KOVALSKI SANTANA <124692976+anellykovalski@users.noreply.github.com>
2023-11-18 21:42:28 -0300

    tela de início do usuário analista

---
+++ pages/cadastroCidade.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../styles.css';
+
+
+function cadastroCidade() {
+  return (
+    
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form" action="http://127.0.0.1:8000/processarCadastroCidade/" method="post">
+            <span className="login-form-title">Cadastro de Cidade</span>
+            <span className="login-form-title">
+            </span>
+  
+            <div className='wrap-input'>
+              <input className='input' name='nomeCidade' type='text' placeholder='Nome da Cidade' required/>
+            </div>
+            <div className='wrap-input'>
+              <input className='input' name='idCidade' type='text' placeholder='id' required/>
+            </div>
+
+            
+            <div className='container-login-form-btn'>
+              <button className='logon-form-btn' type='submit'>
+                CADASTRAR
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default cadastroCidade;
